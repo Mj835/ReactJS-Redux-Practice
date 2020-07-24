e5c26293dcd12c1952fd86193419cae499f4831c
Muskan Jaiswal <muskanmahak777@gmail.com>
2020-07-24 18:59:26 +0530

    Update index.js

---
+++ seasons/src/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // This is the only time we do direct assignment to this.state
+    this.state = { lat: null, errorMessage: "" };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        // we called setState!!!
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
+    );
+  }
+
+  //React says we have to define render!!
+  render() {
+      if (this.state.errorMessage && !this.state.lat) {
+          return <div>Error: {this.state.errorMessage}</div>;
+      }
+
+      if (!this.state.errorMessage && this.state.lat) {
+          return <div>Latitude: {this.state.lat}</div>;
+      }
+
+      return <div>Loading!</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
