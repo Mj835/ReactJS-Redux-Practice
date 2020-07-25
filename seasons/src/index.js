be6fb61b431d753d7423723eba734d0d0ef59ad5
Muskan Jaiswal <muskanmahak777@gmail.com>
2020-07-25 19:35:56 +0530

    Completed Lifecycle Method

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,35 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  //Below state changes to constructor in Babel
+  state = { lat: null, errorMessage: "" };
 
-    // This is the only time we do direct assignment to this.state
-    this.state = { lat: null, errorMessage: "" };
-
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        // we called setState!!!
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err) => {
-        this.setState({ errorMessage: err.message });
-      }
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
     );
   }
 
-  //React says we have to define render!!
+  renderContent() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+
+    return <Spinner message="Please allow location request..." />;
+  }
+
   render() {
-      if (this.state.errorMessage && !this.state.lat) {
-          return <div>Error: {this.state.errorMessage}</div>;
-      }
-
-      if (!this.state.errorMessage && this.state.lat) {
-          return <div>Latitude: {this.state.lat}</div>;
-      }
-
-      return <div>Loading!</div>;
+    return <div>{this.renderContent()}</div>;
   }
 }
 
